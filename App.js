9a9f8305717877472d17ac66e62bac40af381f0b
sakshamthukral <thukral.saksham132@gmail.com>
2024-06-26 11:37:54 -0400

    episode-3

--- App.js
+++ App.js
@@ -1,30 +1,37 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// const heading = React.createElement("h1", {id:"heading"},"Hello World from React!")
-// console.log(heading) // object
-// const root = ReactDOM.createRoot(document.getElementById("root")); 
-// root.render(heading); // will convert the JS object to the HTML element and render it to the DOM
+// JSX :- JavaScript syntax 
+// React Element
+const title = (
+    <h1 className="head" tabIndex="1">
+        Namaste React Using JSX🚀
+        </h1>
+    );
 
-// {/* <div id="parent">
-//     <div id="child1">
-//         <h1>This it the Final Heading</h1>
-//     </div>
+// React Functional Component
+const Title = () => (
+    <h1 className="head" tabIndex="1">
+        Namaste React I'm Title Component🚀
+    </h1>
+);
 
-// </div> */}
+// React Component
+// const HeadingCompoent1 = () => {
+//     return <h1 className="head">Namaste React Functional Component🚀</h1>
+// }
+
+const number = 10000;
+const HeadingComponent2 = () => (
+<div id="container">
+    {Title()}
+    <h1 className="heading">
+        Namaste React Functional Component🚀
+    </h1>
+</div>
+);
 
 
-// So it's like a JS object inside another JS object inside another JS object at the end
-headingObject1 = React.createElement("h1",{}, "This is the h1 Tag from Namaste React");
-headingObject2 = React.createElement("h2",{}, "This is the h2 Tag");
-
-childObject1 = React.createElement("div", {id:"child1"}, [headingObject1, headingObject2]);
-childObject2 = React.createElement("div", {id:"child2"}, [headingObject1, headingObject2]);
-parentObject = React.createElement("div", {id:"parent"}, [childObject1, childObject2]);
-root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parentObject); // It will replace the existing content of the root element with the new content insted of appending it. 
-// ReactElement(Object) => HTML (Browser Understands)
-
-
-// JSX
-// But the above code as you can see seems to be bit complex, instead HTML looks more simpler, but we usually don't use React in this way, it's just to explain the core of the react. We use JSX to write the code in a more simpler way.
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// jsxRoot.render(heading);
+root.render(<HeadingComponent2 />);
